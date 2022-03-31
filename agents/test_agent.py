0e98e9c6350fd6609ce2c36250391e4efb46e3e4
cWetaski <charles.wetaski@mail.mcgill.ca>
2022-03-31 16:20:04 -0400

    added gameplayer.py, test_agent.py

---
+++ agents/test_agent.py
@@ -0,0 +1,61 @@
+# Student agent: Add your own agent here
+from agents.agent import Agent
+from store import register_agent
+import sys
+
+
+@register_agent("test_agent")
+class TestAgent(Agent):
+ 
+    def __init__(self):
+        super(TestAgent, self).__init__()
+        self.name = "TestAgent"
+        self.dir_map = {
+            "u": 0,
+            "r": 1,
+            "d": 2,
+            "l": 3,
+        }
+
+    def step(self, chess_board, my_pos, adv_pos, max_step):
+        self.get_valid_moves(chess_board, my_pos, max_step)
+        text = input("Your move (x,y,dir) or input q to quit: ")
+        
+        while len(text.split(",")) != 3 and "q" not in text.lower():
+            print("Wrong Input Format!")
+            text = input("Your move (x,y,dir) or input q to quit: ")
+        if "q" in text.lower():
+            print("Game ended by user!")
+            sys.exit(0)
+        x, y, dir = text.split(",")
+        x, y, dir = x.strip(), y.strip(), dir.strip()
+        x, y = int(x), int(y)
+        while not self.check_valid_input(
+            x, y, dir, chess_board.shape[0], chess_board.shape[1]
+        ):
+            print(
+                "Invalid Move! (x, y) should be within the board and dir should be one of u,r,d,l."
+            )
+            text = input("Your move (x,y,dir) or input q to quit: ")
+            while len(text.split(",")) != 3 and "q" not in text.lower():
+                print("Wrong Input Format!")
+                text = input("Your move (x,y,dir) or input q to quit: ")
+            if "q" in text.lower():
+                print("Game ended by user!")
+                sys.exit(0)
+            x, y, dir = text.split(",")
+            x, y, dir = x.strip(), y.strip(), dir.strip()
+            x, y = int(x), int(y)
+        my_pos = (x, y)
+        return my_pos, self.dir_map[dir]
+
+    def check_valid_input(self, x, y, dir, x_max, y_max):
+        return 0 <= x < x_max and 0 <= y < y_max and dir in self.dir_map
+
+    def get_valid_moves(self, chess_board, my_pos, max_step):
+        print(my_pos)
+        print(chess_board.shape)
+        
+
+    
+
---
+++ gameplayer.py
@@ -0,0 +1,11 @@
+import simulator
+
+args = simulator.get_args()
+args.player_1 = "test_agent"
+args.player_2 = "random_agent"
+s1 = simulator.Simulator(args)
+result = s1.run()
+
+
+
+
