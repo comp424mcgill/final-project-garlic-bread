f6f10aa6ebbd5a2210729e4cd40e3b220fa058f0
cWetaski <charles.wetaski@mail.mcgill.ca>
2022-04-07 00:44:24 -0400

    Added some more helper functions incl count_edges, get_empty_edges, and started working on a get_stupid_steps funciton to identify bad moves

--- agents/test_agent.py
+++ agents/test_agent.py
@@ -1,5 +1,6 @@
 
 # Student agent: Add your own agent here
+from operator import truediv
 from agents.agent import Agent
 from store import register_agent
 import sys
@@ -19,19 +20,28 @@ class TestAgent(Agent):
             "l": 3,
         }
         self.moves = ((-1, 0), (0, 1), (1, 0), (0, -1)) # moves as defined in world.py (useful for reusing world.py code)
-        self.opposites = {0: 2, 1: 3, 2: 0, 3: 1} # opposite moves as defined in world.py
+        self.opposites = {0: 2, 1: 3, 2: 0, 3: 1} # opposite directions as defined in world.py
 
     def step(self, chess_board, my_pos, adv_pos, max_step):
         valid_steps = self.get_valid_steps(chess_board, my_pos, adv_pos, max_step)
         terminal_steps = set()
+        stupid_steps = set()
         for step in valid_steps:
             is_endgame, is_winner = self.sim_move(chess_board,step,adv_pos)[1:3]
+            num_edges = self.count_edges(chess_board,(step[0],step[1]))
             if(is_endgame == True):
                 terminal_steps.add(tuple((step,is_winner)))
+            if(num_edges == 3):
+                stupid_steps.add(step)
         print("Valid Steps:")
         print(valid_steps)
         print("Terminal Steps:")
         print(terminal_steps)
+        print("Stupid Steps:")
+        print(stupid_steps)
+        not_stupid_steps = self.filter_stupid_steps(chess_board,valid_steps,adv_pos,max_step)
+        print(not_stupid_steps)
+
         text = input("Your move (x,y,dir) or input q to quit: ")
         
         while len(text.split(",")) != 3 and "q" not in text.lower():
@@ -67,7 +77,7 @@ class TestAgent(Agent):
 
     def get_valid_steps(self, chess_board, my_pos, adv_pos, max_step): # returns set (can change to list if necessary) of valid steps from current position
         board_size = chess_board.shape[0]
-        end_posits = []
+        end_posits = [my_pos]
         valid_steps = []
         for n in range(1,max_step+1):
             for r_dist in range(0,n+1):
@@ -85,10 +95,10 @@ class TestAgent(Agent):
         # filter steps which leave boundary or end in adversary's location
         end_posits = set(filter(lambda end_pos: end_pos[0] < board_size and end_pos[1] < board_size and end_pos[0] >= 0 and end_pos[1] >= 0 and end_pos != adv_pos, end_posits)) 
         for end_pos in end_posits:
-            valid_steps.append(tuple((end_pos[0],end_pos[1],0)))
-            valid_steps.append(tuple((end_pos[0],end_pos[1],1)))
-            valid_steps.append(tuple((end_pos[0],end_pos[1],2)))
-            valid_steps.append(tuple((end_pos[0],end_pos[1],3)))
+            for dir in range(0,4):
+                if chess_board[end_pos[0],end_pos[1],dir]:
+                    continue
+                valid_steps.append(tuple((end_pos[0],end_pos[1],dir)))
         valid_steps = set(filter(lambda move: self.check_valid_step(np.asarray(my_pos),[move[0],move[1]],adv_pos, move[2], chess_board, max_step),valid_steps))
         return valid_steps
     
@@ -111,6 +121,7 @@ class TestAgent(Agent):
             return False
         if np.array_equal(start_pos, end_pos):
             return True
+
         # BFS
         state_queue = [(start_pos, 0)]
         visited = {tuple(start_pos)}
@@ -123,7 +134,7 @@ class TestAgent(Agent):
             for dir, move in enumerate(self.moves):
                 if chess_board[r, c, dir]:
                     continue
-                next_pos = cur_pos + move
+                next_pos = (cur_pos[0] + move[0],cur_pos[1]+move[1])
                 if np.array_equal(next_pos, adv_pos) or tuple(next_pos) in visited:
                     continue
                 if np.array_equal(next_pos, end_pos):
@@ -145,14 +156,19 @@ class TestAgent(Agent):
         is_endgame : bool
             Whether the game ends
         is_winner : bool
-            (if is_endgame == true) Whether the player who made the move wins
-            (if is_endgame == false or if game is a tie) None
+            (if is_endgame == True) Whether the player who made the move wins
+            (if is_endgame == False or if game is a tie) None
         """
+
         # create copy of the chess_board to view the new gamestate
         chess_board_copy = deepcopy(chess_board) 
         
         # apply move to copied chess_board
         self.set_barrier(chess_board_copy,move[0],move[1],move[2])
+
+        # check stupid end
+        if self.count_edges(chess_board_copy,(move[0],move[1])) == 4:
+            return chess_board_copy, True, False
 
         # check if move ends game
         cur_pos = (move[0],move[1])
@@ -220,6 +236,51 @@ class TestAgent(Agent):
             is_p0_winner = False
         return True, is_p0_winner
 
+    def count_edges(self,chess_board,pos):
+        count = 0
+        for dir in range(0,4):
+            if chess_board[pos[0],pos[1],dir]:
+                count = count + 1
+        return count
+
+    def get_empty_edges(self,chess_board,pos):
+        empty_edges = []
+        for dir in range(0,4):
+            if not (chess_board[pos[0],pos[1],dir]):
+                empty_edges.append(dir)
+        return empty_edges
+
+
+
+    def get_stupid_steps(self,chess_board,steps,adv_pos,max_step):
+        """
+        Returns stupid steps
+        In implementation it will make more sense for it to return steps which aren't stupid,
+        but for now this makes it easier to see if it is correctly identifying "stupid" steps
+        """
+        filtered_steps = deepcopy(steps)
+        chess_board_copy = deepcopy(chess_board)
+
+        # stupid condition 1: instantly makes us lose
+        # No need to query this, since it will already be queried when checking for terminal steps
+
+        # stupid condition 2: put 3 walls around us when adversary is in range
+        def check_stupid_2(chess_board,step,adv_pos,max_step):
+            r,c,dir = step
+            empty_edges = self.get_empty_edges(chess_board,(r,c))
+            if (self.count_edges(chess_board_copy,(r,c)) == 2): # might be stupid
+                empty_edges.remove(dir)
+                empty_dir = empty_edges[0]
+                move = self.moves[empty_dir]
+                win_pos = (r+move[0],c+move[1])
+                if self.check_valid_step(tuple(adv_pos),win_pos,(r,c),self.opposites[empty_dir],chess_board,max_step):
+                    return True
+            return False
+
+        filtered_steps = set(filter(lambda step: check_stupid_2(chess_board,step,adv_pos,max_step),steps))
+        return filtered_steps
+
+    
 
 
 
--- play.py
+++ play.py
@@ -4,6 +4,7 @@ args = simulator.get_args()
 args.player_1 = "test_agent"
 args.player_2 = "random_agent"
 args.display = True
+args.board_size = 5
 s1 = simulator.Simulator(args)
 result = s1.run()
 
