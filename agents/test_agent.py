77065938e5a1e9d68f86545ee8694035be808e44
cWetaski <charles.wetaski@mail.mcgill.ca>
2022-04-06 22:07:13 -0400

    Added functions to test_agent.py to determine if a proposed step will end the game and (if the game is ended) who wins

--- agents/test_agent.py
+++ agents/test_agent.py
@@ -4,6 +4,7 @@ from agents.agent import Agent
 from store import register_agent
 import sys
 import numpy as np
+from copy import deepcopy
 
 @register_agent("test_agent")
 class TestAgent(Agent):
@@ -17,9 +18,20 @@ class TestAgent(Agent):
             "d": 2,
             "l": 3,
         }
+        self.moves = ((-1, 0), (0, 1), (1, 0), (0, -1)) # moves as defined in world.py (useful for reusing world.py code)
+        self.opposites = {0: 2, 1: 3, 2: 0, 3: 1} # opposite moves as defined in world.py
 
     def step(self, chess_board, my_pos, adv_pos, max_step):
-        self.get_valid_moves(chess_board, my_pos, adv_pos, max_step)
+        valid_steps = self.get_valid_steps(chess_board, my_pos, adv_pos, max_step)
+        terminal_steps = set()
+        for step in valid_steps:
+            is_endgame, is_winner = self.sim_move(chess_board,step,adv_pos)[1:3]
+            if(is_endgame == True):
+                terminal_steps.add(tuple((step,is_winner)))
+        print("Valid Steps:")
+        print(valid_steps)
+        print("Terminal Steps:")
+        print(terminal_steps)
         text = input("Your move (x,y,dir) or input q to quit: ")
         
         while len(text.split(",")) != 3 and "q" not in text.lower():
@@ -53,43 +65,32 @@ class TestAgent(Agent):
     def check_valid_input(self, x, y, dir, x_max, y_max):
         return 0 <= x < x_max and 0 <= y < y_max and dir in self.dir_map
 
-    def get_valid_moves(self, chess_board, my_pos, adv_pos, max_step): # returns set (can change to list if necessary) of valid moves from current position
-        print(my_pos)
-        print(max_step)
+    def get_valid_steps(self, chess_board, my_pos, adv_pos, max_step): # returns set (can change to list if necessary) of valid steps from current position
         board_size = chess_board.shape[0]
-        print('board size is %d' % (board_size))
         end_posits = []
+        valid_steps = []
         for n in range(1,max_step+1):
             for r_dist in range(0,n+1):
-
                 c_dist = n - r_dist
-                print('%d %d %d' % (n,r_dist,c_dist))
                 if r_dist == 0:
-                    cur_moves = [(my_pos[0],my_pos[1] + c_dist),(my_pos[0],my_pos[1] - c_dist)]
+                    cur_steps = [(my_pos[0],my_pos[1] + c_dist),(my_pos[0],my_pos[1] - c_dist)]
                 elif c_dist == 0:
-                    cur_moves = [(my_pos[0] + r_dist,my_pos[1]),(my_pos[0] - r_dist,my_pos[1])]
+                    cur_steps = [(my_pos[0] + r_dist,my_pos[1]),(my_pos[0] - r_dist,my_pos[1])]
                 else:
-                    cur_moves = [(my_pos[0] + r_dist,my_pos[1] + c_dist),(
+                    cur_steps = [(my_pos[0] + r_dist,my_pos[1] + c_dist),(
                             my_pos[0] + r_dist,my_pos[1] - c_dist),(
                             my_pos[0] - r_dist,my_pos[1] + c_dist),(
                             my_pos[0] - r_dist,my_pos[1] - c_dist)]
-                print(cur_moves)
-                end_posits.extend(cur_moves)
-        
-        print(end_posits)
-        end_posits = set(filter(lambda end_pos: end_pos[0] < board_size and end_pos[1] < board_size and end_pos[0] >= 0 and end_pos[1] >= 0 and end_pos != adv_pos, end_posits)) # filter moves which leave boundary or end in adversary's location
-        print('filtered')
-        print(end_posits)
-        moves = []
+                end_posits.extend(cur_steps)
+        # filter steps which leave boundary or end in adversary's location
+        end_posits = set(filter(lambda end_pos: end_pos[0] < board_size and end_pos[1] < board_size and end_pos[0] >= 0 and end_pos[1] >= 0 and end_pos != adv_pos, end_posits)) 
         for end_pos in end_posits:
-            moves.append(tuple((end_pos[0],end_pos[1],0)))
-            moves.append(tuple((end_pos[0],end_pos[1],1)))
-            moves.append(tuple((end_pos[0],end_pos[1],2)))
-            moves.append(tuple((end_pos[0],end_pos[1],3)))
-
-        moves = set(filter(lambda move: self.check_valid_step(np.asarray(my_pos),[move[0],move[1]],adv_pos, move[2], chess_board, max_step),moves))
-        print(moves)
-        return moves
+            valid_steps.append(tuple((end_pos[0],end_pos[1],0)))
+            valid_steps.append(tuple((end_pos[0],end_pos[1],1)))
+            valid_steps.append(tuple((end_pos[0],end_pos[1],2)))
+            valid_steps.append(tuple((end_pos[0],end_pos[1],3)))
+        valid_steps = set(filter(lambda move: self.check_valid_step(np.asarray(my_pos),[move[0],move[1]],adv_pos, move[2], chess_board, max_step),valid_steps))
+        return valid_steps
     
     def check_valid_step(self, start_pos, end_pos, adv_pos, barrier_dir, chess_board, max_step): # reused from world.py (modified to work in this context)
         """
@@ -104,42 +105,121 @@ class TestAgent(Agent):
         barrier_dir : int
             The direction of the barrier.
         """
-        print(type(adv_pos))
-        print(type(start_pos))
-        moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
-
         # Endpoint already has barrier or is boarder
         r, c = end_pos
         if chess_board[r, c, barrier_dir]:
             return False
         if np.array_equal(start_pos, end_pos):
             return True
-
         # BFS
         state_queue = [(start_pos, 0)]
         visited = {tuple(start_pos)}
         is_reached = False
         while state_queue and not is_reached:
             cur_pos, cur_step = state_queue.pop(0)
-            print(cur_pos)
             r, c = cur_pos
             if cur_step == max_step:
                 break
-            for dir, move in enumerate(moves):
+            for dir, move in enumerate(self.moves):
                 if chess_board[r, c, dir]:
                     continue
-
                 next_pos = cur_pos + move
                 if np.array_equal(next_pos, adv_pos) or tuple(next_pos) in visited:
                     continue
                 if np.array_equal(next_pos, end_pos):
                     is_reached = True
                     break
-
                 visited.add(tuple(next_pos))
                 state_queue.append((next_pos, cur_step + 1))
-
         return is_reached
+
+    def sim_move(self,chess_board,move,adv_pos):
+        """
+        Assumption is that move the move is valid
+        Will add the move to a copy of the chess_board, and check if the move ends the game
+        
+        Returns
+        -------
+        chess_board_copy : chess_board
+            Copy of chess_board with move applied
+        is_endgame : bool
+            Whether the game ends
+        is_winner : bool
+            (if is_endgame == true) Whether the player who made the move wins
+            (if is_endgame == false or if game is a tie) None
+        """
+        # create copy of the chess_board to view the new gamestate
+        chess_board_copy = deepcopy(chess_board) 
+        
+        # apply move to copied chess_board
+        self.set_barrier(chess_board_copy,move[0],move[1],move[2])
+
+        # check if move ends game
+        cur_pos = (move[0],move[1])
+        is_endgame, is_winner = self.check_endgame(chess_board_copy,cur_pos,adv_pos)
+        return chess_board_copy, is_endgame, is_winner
+
+    def set_barrier(self, chess_board, r, c, dir): # adapted from world.py function of same name
+        # Set the barrier to True
+        chess_board[r, c, dir] = True
+        # Set the opposite barrier to True
+        move = self.moves[dir]
+        chess_board[r + move[0], c + move[1], self.opposites[dir]] = True
+
+    def check_endgame(self,chess_board,p0_pos,p1_pos): # adapted from world.py function of same name
+        """
+        Check if a game state is terminal and return who wins if so
+        Does not return score since it is not useful
+
+        Returns
+        -------
+        is_endgame : bool
+            Whether the game ends.
+        is_p0_winner : bool
+            Whether p0 wins (if is_endgame == false or if there is a tie, returns None)
+        """
+        # Union-Find
+        father = dict()
+        for r in range(chess_board.shape[0]):
+            for c in range(chess_board.shape[0]):
+                father[(r, c)] = (r, c)
+
+        def find(pos):
+            if father[pos] != pos:
+                father[pos] = find(father[pos])
+            return father[pos]
+
+        def union(pos1, pos2):
+            father[pos1] = pos2
+
+        for r in range(chess_board.shape[0]):
+            for c in range(chess_board.shape[0]):
+                for dir, move in enumerate(
+                    self.moves[1:3]
+                ):  # Only check down and right
+                    if chess_board[r, c, dir + 1]:
+                        continue
+                    pos_a = find((r, c))
+                    pos_b = find((r + move[0], c + move[1]))
+                    if pos_a != pos_b:
+                        union(pos_a, pos_b)
+
+        for r in range(chess_board.shape[0]):
+            for c in range(chess_board.shape[0]):
+                find((r, c))
+        p0_r = find(tuple(p0_pos))
+        p1_r = find(tuple(p1_pos))
+        p0_score = list(father.values()).count(p0_r)
+        p1_score = list(father.values()).count(p1_r)
+        if p0_r == p1_r:
+            return False, None
+        is_p0_winner = None
+        if p0_score > p1_score:
+            is_p0_winner = True
+        elif p0_score < p1_score:
+            is_p0_winner = False
+        return True, is_p0_winner
+
 
 
 
